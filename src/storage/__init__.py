79972ccc58d78bb86d7d9c7c4a6b92352d8efba9
Joshuet <joshuetd@gmail.com>
2026-06-24 20:12:26 -0500

    ajustes en el formato

--- src/storage/__init__.py
+++ src/storage/__init__.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+
 def guardar_parquet(df, ruta="data/TRM.parquet"):
     Path(ruta).parent.mkdir(parents=True, exist_ok=True)  # crea data/ si no existe
     df.write_parquet(ruta)
